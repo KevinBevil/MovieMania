a815059c483f205d9aacc4c695aa6f7acfe96e85
Kevin Bevil <kbevil@gmail.com>
2019-06-15 12:17:28 -0400

    Integrating Gmail Oauth into UI

--- public/js/components/vueHeader.js
+++ public/js/components/vueHeader.js
@@ -1,16 +1,15 @@
 var header = new Vue({
-    el: '[vue="header"]',
-    data() {
-        return {
-            
-        }
-    },
-    template: `
+  el: '[vue="header"]',
+  data() {
+    return {};
+  },
+  template: `
         <div class="row">
             <div class="col-12 d-flex justify-content-between align-items-center">
                 <a href="#" id="logo">Movie Mania</a>
-                <div class="text-right">
-                    <a href="#" id="log-in">Log In | Sign Up</a>
+                <div class="text-right signin-nav">
+                    <a href="#" onclick="signOut();">Sign out</a>
+                    <div id="my-signin2"></div>
                     <nav>
                         <ul class="d-flex">
                             <li><a href="#">Recently Rated</a></li>
