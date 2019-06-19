9309521a42ea6d47537b60617051c06cf941d31c
Kevin Bevil <kbevil@gmail.com>
2019-06-18 21:10:10 -0400

    Fixing recently.. and movie.js

--- models/movie.js
+++ models/movie.js
@@ -10,6 +10,26 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING(100),
       allowNull: false
     },
+    moviePoster: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    movieRuntime: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    movieRating: {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    },
+    movieActors: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    movieDirector: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
     movieGenre: {
       type: DataTypes.STRING(100),
       allowNull: false
@@ -26,10 +46,30 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING(30),
       allowNull: false
     },
+    movieProduction: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    movieWriter: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    movieCountry: {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    },
+    movieLanguage: {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    },
     watched: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
       defaultValue: false
+    },
+    userRating: {
+      type: DataTypes.STRING(30),
+      allowNull: true
     }
   });
   return Movie;
--- public/js/recentlyRated.js
+++ public/js/recentlyRated.js
@@ -1,45 +1,65 @@
-$(document).ready(function() {
-  /* 1. Visualizing things on Hover - See next part for action on click */
-  $(".star-holder")
-    .on("mouseover", ".star", function() {
-      // The star currently selected
-      var ratingValue = parseInt($(this).data("value"), 10); // The star currently mouse on
+$(document).ready(function () {
 
-      // Now highlight all the stars that's not after the current hovered star
-      $(this)
-        .parent()
-        .children("li.star")
-        .each(function(e) {
-          if (e < ratingValue) {
-            $(this).addClass("hover");
-          } else {
-            $(this).removeClass("hover");
-          }
-        });
-    })
-    .on("mouseout", ".star", function() {
-      $(this)
-        .parent()
-        .children("li.star")
-        .each(function(e) {
-          $(this).removeClass("hover");
-        });
-    });
-  /* 2. Action to perform on click */
-  $(".star-holder").on("click", ".star", function() {
+  /* 1. Visualizing things on Hover - See next part for action on click */
+  $('.star-holder').on('mouseover', '.star', function () {
     // The star currently selected
-    var ratingValue = parseInt($(this).data("value"), 10);
-    var stars = $(this)
-      .parent()
-      .children("li.star");
+    var ratingValue = parseInt($(this).data('value'), 10); // The star currently mouse on
+
+    // Now highlight all the stars that's not after the current hovered star
+    $(this).parent().children('li.star').each(function (e) {
+      if (e < ratingValue) {
+        $(this).addClass('hover');
+      }
+      else {
+        $(this).removeClass('hover');
+      }
+    });
+
+  }).on('mouseout', '.star', function () {
+    $(this).parent().children('li.star').each(function (e) {
+      $(this).removeClass('hover');
+    });
+  });
+  /* 2. Action to perform on click */
+  $('.star-holder').on('click', '.star', function () {
+    // The star currently selected
+    var ratingValue = parseInt($(this).data('value'), 10);
+    var stars = $(this).parent().children('li.star');
 
     for (i = 0; i < stars.length; i++) {
-      $(stars[i]).removeClass("selected");
+      $(stars[i]).removeClass('selected');
     }
 
     for (i = 0; i < ratingValue; i++) {
-      $(stars[i]).addClass("selected");
+      $(stars[i]).addClass('selected');
     }
     console.log(`You rated this ${ratingValue} stars.`);
   });
+
+});
+
+
+Vue.component("genStars", {
+  props: ["userRating"],
+  template: `
+    <div>You rated this:
+      <ul class='stars'>
+        <li v-bind:class="{selected: (userRating >= 1)}" class='star' title='Poor' data-value='1'>
+            <i class='fas fa-star'></i>
+        </li>
+        <li v-bind:class="{selected: (userRating >= 2)}"class='star' title='Fair' data-value='2'>
+            <i class='fas fa-star'></i>
+        </li>
+        <li v-bind:class="{selected: (userRating >= 3)}"class='star' title='Good' data-value='3'>
+            <i class='fas fa-star'></i>
+        </li>
+        <li v-bind:class="{selected: (userRating >= 4)}"class='star' title='Excellent' data-value='4'>
+            <i class='fas fa-star'></i>
+        </li>
+        <li v-bind:class="{selected: (userRating >= 5)}"class='star' title='WOW!!!' data-value='5'>
+            <i class='fas fa-star'></i>
+        </li>
+      </ul>
+    </div>
+  `
 });
