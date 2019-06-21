9040451bc8f3b9e8186906a27ad3e8eac0cc1601
Joe Keegan <jwkeeg@comcast.net>
2019-06-21 08:33:53 -0500

    fixed merge issue in vueHeader

--- public/js/components/vueHeader.js
+++ public/js/components/vueHeader.js
@@ -8,15 +8,7 @@ var header = new Vue({
             <div class="col-12 d-flex justify-content-between align-items-center">
                 <a href="/" id="logo">Movie Mania</a>
                 <div class="text-right signin-nav">
-<<<<<<< HEAD
                     <div id="my-signoff2"></div>
-=======
-                    <div id="my-signoff2">
-                        <a class="mb-2" href="#" onclick="signOut();">Sign out</a>
-                        Logged in as <span id="user-name"></span>
-                        <img id="user-image">
-                    </div>
->>>>>>> 2853eedc51829ba4aebbdf3833dde62f3760393d
                     <div id="my-signin2"></div>
                 </div>
             </div>
